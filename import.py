a7f28610d1288f99a21a2b79862326566050435a
grandpariley <rileyherman@tuta.io>
2022-02-07 20:48:26 -0600

    qs does everything for me yay

--- import.py
+++ import.py
@@ -1,21 +1,25 @@
 import csv
 import json
+import os
 import time
 
-import yfinance as yf
+import numpy as np
+import quantstats as qs
 
 
 def get_companies():
     companies = []
     with open('TSX Data - 2022-02-06.csv', 'r') as tsx_file:
-        tsx = csv.reader(tsx_file, delimiter=";")
+        tsx = csv.reader(tsx_file)
         for company in tsx:
             companies.append(company[0])
     return companies
 
 
 def save_to_file(d, file_index):
-    with open('data' + str(file_index) + '.json', 'w') as output:
+    if not os.path.exists("output"):
+        os.mkdir("output")
+    with open('output/data' + str(file_index) + '.json', 'w') as output:
         json.dump(d, output, indent=4)
 
 
@@ -23,18 +27,21 @@ def save_company_data(companies):
     infos = []
     file_index, count_enough_info, count_not_enough_info = 0, 0, 0
     for company in companies:
-        print("Gathering data for " + company)
+        print("Gathering output for " + company)
         time.sleep(1)  # so I don't overload yahoo finance
-        ticker = yf.Ticker(company)
-        info = ticker.info
-        if "threeYearAverageReturn" not in info or not info["threeYearAverageReturn"]:
-            count_not_enough_info += 1
-            print("\tnot enough information! count at " + str(count_not_enough_info))
+        stock = qs.utils.download_returns(company)
+        rt = qs.stats.avg_return(stock)
+        cvar = qs.stats.cvar(stock)
+        var = qs.stats.var(stock)
+        if np.isnan(rt) or np.isnan(cvar) or np.isnan(var):
             continue
-        infos.append(info)
-        count_enough_info += 1
-        print("\tenough information! count at " + str(count_enough_info))
-        if len(infos) > 5:
+        infos.append({
+            'ticker': company,
+            'return': rt,
+            'cvar': cvar,
+            'var': var
+        })
+        if len(infos) > 100:
             print("Dumping to file")
             save_to_file(infos, file_index)
             file_index += 1
