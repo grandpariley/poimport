188a6117789b58c5b8d37497235fdc36e8bd44ba
grandpariley <rileyherman@tuta.io>
2022-02-12 12:35:36 -0600

    try except block just to piss off everyone who reads this code

--- import.py
+++ import.py
@@ -28,32 +28,39 @@ def save_company_data(companies):
     file_index = 0
     for company in companies:
         print("Gathering output for " + company)
-        stock = qs.utils.download_returns(company)
-        rt = qs.stats.avg_return(stock)
-        cvar = qs.stats.cvar(stock)
-        var = qs.stats.var(stock)
-        if np.isnan(rt) or np.isnan(cvar) or np.isnan(var):
+        try:
+            stock = qs.utils.download_returns(company)
+            rt = qs.stats.expected_return(stock)
+            cvar = qs.stats.cvar(stock)
+            var = qs.stats.var(stock)
+            if np.isnan(rt) or np.isnan(cvar) or np.isnan(var):
+                continue
+            ticker = yf.Ticker(company)
+            environment, social, governance = float('nan'), float('nan'), float('nan')
+            price = ticker.info['previousClose']
+            if np.isnan(price):
+                continue
+            if ticker.sustainability is not None:
+                environment = ticker.sustainability.get('Value').get('environmentScore')
+                governance = ticker.sustainability.get('Value').get('governanceScore')
+                social = ticker.sustainability.get('Value').get('socialScore')
+            infos.append({
+                'ticker': company,
+                'price': price,
+                'return': rt,
+                'cvar': cvar,
+                'var': var,
+                'environment': None if np.isnan(environment) else environment,
+                'governance': None if np.isnan(governance) else governance,
+                'social': None if np.isnan(social) else social,
+            })
+        except:
             continue
-        ticker = yf.Ticker(company)
-        environment, social, governance = float('nan'), float('nan'), float('nan')
-        if ticker.sustainability is not None:
-            environment = ticker.sustainability.get('Value').get('environmentScore')
-            governance = ticker.sustainability.get('Value').get('governanceScore')
-            social = ticker.sustainability.get('Value').get('socialScore')
-        infos.append({
-            'ticker': company,
-            'return': rt,
-            'cvar': cvar,
-            'var': var,
-            'environment': None if np.isnan(environment) else environment,
-            'governance': None if np.isnan(governance) else governance,
-            'social': None if np.isnan(social) else social,
-        })
-        if len(infos) > 200:
-            print("Dumping to file")
-            save_to_file(infos, file_index)
-            file_index += 1
-            infos = []
+        # if len(infos) > 1:
+        #     print("Dumping to file")
+        #     save_to_file(infos, file_index)
+        #     file_index += 1
+        #     infos = []
     save_to_file(infos, file_index)
 
 
