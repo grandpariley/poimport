9604eb00f9c67b636cc9c0d50cea2e2615254694
grandpariley <rileyherman@duck.com>
2024-07-26 15:54:55 -0600

    added scaling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 
 import numpy as np
@@ -76,18 +77,20 @@ def get_risk(symbol):
 
 
 def get_esg(symbol, ticker):
-    environment, social, governance = np.nan, np.nan, np.nan
+    environment, governance, social = np.nan, np.nan, np.nan
     if not isinstance(ticker.esg_scores.get(symbol), str):
         environment = ticker.esg_scores.get(symbol).get('environmentScore')
-        governance = ticker.esg_scores.get(symbol).get('governanceScore')
         social = ticker.esg_scores.get(symbol).get('socialScore')
-    return environment, governance, social
+        governance = ticker.esg_scores.get(symbol).get('governanceScore')
+    return environment, social, governance
 
 
 def get_capm_expected_return(symbol, ticker):
     if not isinstance(ticker.summary_detail.get(symbol), dict):
         raise ValueError('no expected return - ' + symbol)
     beta = ticker.summary_detail.get(symbol).get('beta')
+    if symbol == '^GSPTSE':
+        beta = 0.5  # the S&P TSX index should have a beta of 0.5 by definition
     if beta is None or np.isnan(beta):
         raise ValueError('no expected return - ' + symbol)
     return CANADA_RISK_FREE_RATE + (beta * (TSX_EXPECTED_RETURN - CANADA_RISK_FREE_RATE))
@@ -134,7 +137,7 @@ def get_company_data(companies, retry, no_data, fileprefix='', refresh_cache=Tru
             price = get_price(symbol, ticker)
             expected_return = get_capm_expected_return(symbol, ticker)
             cvar, var = get_risk(symbol)
-            environment, governance, social = get_esg(symbol, ticker)
+            environment, social, governance = get_esg(symbol, ticker)
             data[symbol] = {
                 'ticker': symbol,
                 'price': price,
@@ -142,11 +145,11 @@ def get_company_data(companies, retry, no_data, fileprefix='', refresh_cache=Tru
                 'cvar': cvar,
                 'var': var,
                 'environment': None if environment is None or np.isnan(environment) else environment,
-                'governance': None if governance is None or np.isnan(governance) else governance,
                 'social': None if social is None or np.isnan(social) else social,
+                'governance': None if governance is None or np.isnan(governance) else governance,
             }
             success_count += 1
-            if data[symbol]['environment'] or data[symbol]['governance'] or data[symbol]['social']:
+            if data[symbol]['environment'] or data[symbol]['social'] or data[symbol]['governance']:
                 esg_count += 1
             save_data(data, fileprefix)
             print("currently " + str(success_count) + " valid data points with " + str(esg_count) + ' esg data points')
@@ -179,38 +182,63 @@ def save_company_data(companies, fileprefix='', refresh_cache=True):
     return data
 
 
-def scale_esg():
-    with open('output/data.json', 'r') as json_file:
-        data = dict(json.load(json_file))
-        max = {
-            'environment': 0,
-            'social': 0,
-            'governance': 0,
+def scale(in_file='output/data.json', out_file='output/data.json'):
+    maximum = get_max_values(in_file)
+    with open(out_file, 'r') as json_file:
+        out_data = dict(json.load(json_file))
+        for k in out_data:
+            if out_data[k]['cvar']:
+                out_data[k]['cvar'] = out_data[k]['cvar'] / float(maximum['cvar'])
+            if out_data[k]['var']:
+                out_data[k]['var'] = out_data[k]['var'] / float(maximum['var'])
+            if out_data[k]['return']:
+                out_data[k]['return'] = out_data[k]['return'] / float(maximum['return'])
+            if out_data[k]['environment']:
+                out_data[k]['environment'] = out_data[k]['environment'] / float(maximum['environment'])
+            if out_data[k]['social']:
+                out_data[k]['social'] = out_data[k]['social'] / float(maximum['social'])
+            if out_data[k]['governance']:
+                out_data[k]['governance'] = out_data[k]['governance'] / float(maximum['governance'])
+
+    with open(out_file, 'w') as json_file:
+        json.dump(out_data, json_file)
+
+
+@file_cache('max.json')
+def get_max_values(in_file):
+    with open(in_file, 'r') as json_file:
+        in_data = dict(json.load(json_file))
+        maximum = {
+            'cvar': -math.inf,
+            'var': -math.inf,
+            'return': -math.inf,
+            'environment': -math.inf,
+            'social': -math.inf,
+            'governance': -math.inf,
         }
-        for v in data.values():
-            if v['environment'] and v['environment'] > max['environment']:
-                max['environment'] = v['environment']
-            if v['social'] and v['social'] > max['social']:
-                max['social'] = v['social']
-            if v['governance'] and v['governance'] > max['governance']:
-                max['governance'] = v['governance']
-        for k in data:
-            if data[k]['environment']:
-                data[k]['environment'] = data[k]['environment'] / float(max['environment'])
-            if data[k]['social']:
-                data[k]['social'] = data[k]['social'] / float(max['social'])
-            if data[k]['governance']:
-                data[k]['governance'] = data[k]['governance'] / float(max['governance'])
-    with open('output/data.json', 'w') as json_file:
-        json.dump(data, json_file)
+        for v in in_data.values():
+            if v['cvar'] and v['cvar'] > maximum['cvar']:
+                maximum['cvar'] = v['cvar']
+            if v['var'] and v['var'] > maximum['var']:
+                maximum['var'] = v['var']
+            if v['return'] and v['return'] > maximum['return']:
+                maximum['return'] = v['return']
+            if v['environment'] and v['environment'] > maximum['environment']:
+                maximum['environment'] = v['environment']
+            if v['social'] and v['social'] > maximum['social']:
+                maximum['social'] = v['social']
+            if v['governance'] and v['governance'] > maximum['governance']:
+                maximum['governance'] = v['governance']
+    return maximum
 
 
 def main():
     companies = get_companies()
     save_company_data(companies)
-    scale_esg()
+    scale()
     index = ['GSPTSE']
     save_company_data(index, 'index-', False)
+    scale(out_file='output/index-data.json')
 
 
 if __name__ == "__main__":
