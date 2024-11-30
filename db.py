6ba27c86b871e65ac9c2f349902b22744ab1e735
grandpariley <rileyherman@duck.com>
2024-11-30 17:40:47 -0600

    Reapply "refactor"
    This reverts commit 179634a5243adae55e74588f6da6d2ea4c5d87b4.

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import gc
 import os
 
 import motor.motor_asyncio
@@ -29,16 +30,16 @@ async def insert_no_data(nd):
     await no_data.insert_one({'symbol': nd})
 
 
-async def fetch_data():
-    data_as_list = await find_all(data.find(None))
+async def fetch_data(symbol):
+    data_as_list = await find_all(data.find({'symbol': symbol}))
     data_as_dict = dict()
     for d in data_as_list:
         data_as_dict[d['symbol']] = d['data']
-    return data_as_dict
+    return data_as_dict[symbol]
 
 
-def fetch_no_data(query):
-    return find_all(no_data.find(query))
+async def fetch_no_data():
+    return await find_all(no_data.find(None))
 
 
 async def find_all(cursor):
@@ -47,3 +48,17 @@ async def find_all(cursor):
     async for result in cursor:
         results.append(result)
     return results
+
+
+async def count():
+    return await data.count_documents(None)
+
+
+async def symbols():
+    s = []
+    cursor = data.find(None)
+    async for result in cursor:
+        s.append(result['symbol'])
+        del result
+        gc.collect()
+    return s
